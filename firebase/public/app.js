51833d7fafcc9ee3eb004f17a4f3baee32d9fb0e
Dabson <cyphlix@gmail.com>
2019-04-22 20:37:23 +1000

    clean out console.logs

--- firebase/public/app.js
+++ firebase/public/app.js
@@ -3,14 +3,13 @@ document.addEventListener("DOMContentLoaded", event => {
 	const db = firebase.firestore();
 	const productsRef = db.collection('products');
 
-	const query = productsRef.where('price', '>', 10);
+	const query = productsRef
+		.orderBy('price')
+		.where('price', '>=', 10);
 
-	console.log('hi');
 	query.get()
 		.then(products => {
-			console.log('d', products);
 			products.forEach(doc => {
-				console.log('eee');
 				data = doc.data();
 				document.write(`${data.name} at $${data.price}<br>`);
 			})
